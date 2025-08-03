e8d4ff9002859cdcc6dcc175cbdab54718816784
Norahmw21 <norahmw2@gmail.com>
2025-08-03 14:23:19 +0300

    init commit

---
+++ src/main/java/com/company/docreview/DocreviewApplication.java
@@ -0,0 +1,13 @@
+package com.company.docreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocreviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/docreview/DocreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.docreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
